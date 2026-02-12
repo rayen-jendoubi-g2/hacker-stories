e56abffe8cc4387788a32b69a2613b73e76a8b24
rayen jendoubi <rayen.jendoubi@tbs.u-tunis.tn>
2026-02-12 11:31:35 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function sayHello(name) {
+  return `Hello, ${name}! Welcome aboard 🚀`;
+}
+
+function App() {
+  const studentName = "Rayen";
+  const student = {
+    name: "Rayen",
+    age: 21,
+    track: "Finance & Web Development"
+  };
+
+  return (
+    <div>
+      <h1>{sayHello(studentName)}</h1>
+      <p>Course: {courseTitle}</p>
+      <p>Name: {student.name}</p>
+      <p>Age: {student.age}</p>
+      <p>Track: {student.track}</p>
+
+      <label htmlFor="feedback">Your Feedback:</label>
+      <input id="feedback" type="text" />
+    </div>
+  );
+}
+
+export default App
