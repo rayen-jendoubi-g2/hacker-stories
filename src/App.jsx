172c3596e30c96194b73cae8a58ccf58d7bc00dc
rayen jendoubi <rayen.jendoubi@tbs.u-tunis.tn>
2026-03-01 15:26:03 +0100

    week 5

--- src/App.jsx
+++ src/App.jsx
@@ -2,31 +2,66 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+const list = [ 
+  { 
+    title : " react" , 
+    url :"https://react.js.org/",
+    author : "jordan",
+    num_comments : 2,
+    points : 3 , 
+    object_ID:0 ,
+  },
 
-function sayHello(name) {
-  return `Hello, ${name}! Welcome aboard 🚀`;
-}
+   { 
+    title : " redux" , 
+    url :"https://redux.js.org/",
+    author : "dan",
+    num_comments : 2,
+    points : 5 , 
+    object_ID:1 ,
+  }, 
+] ; 
+const App = () =>
+  <div>  
+    <Title> </Title>
+    <Search></Search>
+    <List></List> 
+  </div>
+  
+const List = () => 
+  <ul>
+  {list.map((item) => 
+  {return(
+  <li key={item.object_ID}>
+    <span>
+      < a href = {item.url}>{item.title} </a>
+    </span>
+    <span> {item.author}
+    </span>
+    <span>{item.num_comments}</span>
+    <span>{item.points}</span>
+  </li>); })}
+</ul>
 
-function App() {
-  const studentName = "Rayen";
-  const student = {
-    name: "Rayen",
-    age: 21,
-    track: "Web Development"
-  };
-
+const Search = () => {
+  const handleChange = (event) => {
+ console.log(event.target.value);
+ console.log("input changed");
+};
   return (
     <div>
-      <h1>{sayHello(studentName)}</h1>
-      <p>Course: {courseTitle}</p>  
-      <p>Name: {student.name}</p>
-      <p>Age: {student.age}</p>
-      <p>Track: {student.track}</p>
-
-      <label htmlFor="feedback">Your Feedback:</label>
-      <input id="feedback" type="text" />
+      <label htmlFor="search"> Search :</label>
+      <input id="search" type ="text" onChange={handleChange}></input>
     </div>
   );
 }
-
+  <div>
+    <label htmlFor="search"> Search :</label>
+    <input id="search" type ="texte"></input>
+  </div>
+const Title = () =>
+    <div>
+ <h1> My hacker stories </h1>
+    </div>
+  
 export default App
